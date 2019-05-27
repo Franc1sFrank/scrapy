04eb3b1b0af7c853c400cd2bb3c2b26abd211862
Francis Z. Fang <777+Franc1sFrank@users.noreply.github.com>
2019-05-26 23:38:43 -0400

    scrap

--- scrapy.py
+++ scrapy.py
@@ -1,8 +1,59 @@
 import lxml
 import parsel
+import scrapy
 import w3lib
 import twisted
 import cryptography
 import pyOpenSSL
 
+
 class QiushiSpider(scrapy.Spider):
+    name = "qiushibaike"
+
+def start_requests(self):
+    urls = [
+        'https://www.qiushibaike.com/text/page/1/',
+        'https://www.qiushibaike.com/text/page/2/',
+    ]
+    for url in urls:
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        content_left_div = response.xpath('//*[@id="content-left"]')
+        content_list_div = content_left_div.xpath('./div')
+
+        for content_div in content_list_div:
+            yield {
+                'author': content_div.xpath('./div/a[2]/h2/text()').get(),
+                'content': content_div.xpath('./a/div/span/text()').getall(),
+            }
+
+
+
+haders = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/73.0.3683.86 Chrome/73.0.3683.86 Safari/537.36'
+    }
+
+    def start_requests(self):
+        urls = [
+            'https://www.qiushibaike.com/text/page/12/',
+            'https://www.qiushibaike.com/text/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, headers=self.haders)
+
+scrapy shell /home/wistbean/PycharmProjects/spider/qiushibaike/qiushi-1.html
+
+content_left_div = response.xpath('//[@id="content-left"]')
+
+content_list_div = content_left_div.xpath('./div')
+
+content_div = content_list_div[0]
+
+author = content_div.xpath('./div/a[2]/h2/text()').get()
+print(author)
+
+content = content_div.xpath('./a/div/span/text()').getall()
+print(content)
+
