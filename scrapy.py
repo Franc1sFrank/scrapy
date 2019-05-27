fb944515df9d6db4b824970ed58982f806fb3e03
Francis Z. Fang <777+Franc1sFrank@users.noreply.github.com>
2019-05-26 23:43:59 -0400

    update

--- scrapy.py
+++ scrapy.py
@@ -1,10 +1,9 @@
 import lxml
 import parsel
-import scrapy
 import w3lib
 import twisted
 import cryptography
-import pyOpenSSL
+import scrapy
 
 
 class QiushiSpider(scrapy.Spider):
@@ -35,15 +34,15 @@ haders = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/73.0.3683.86 Chrome/73.0.3683.86 Safari/537.36'
     }
 
-    def start_requests(self):
-        urls = [
-            'https://www.qiushibaike.com/text/page/12/',
-            'https://www.qiushibaike.com/text/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse, headers=self.haders)
+def start_requests(self):
+    urls = [
+        'https://www.qiushibaike.com/text/page/12/',
+        'https://www.qiushibaike.com/text/page/2/',
+    ]
+    for url in urls:
+        yield scrapy.Request(url=url, callback=self.parse, headers=self.haders)
 
-scrapy shell /home/wistbean/PycharmProjects/spider/qiushibaike/qiushi-1.html
+
 
 content_left_div = response.xpath('//[@id="content-left"]')
 
